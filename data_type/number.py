34ccb3f5e645cf635e9707e91dd5c4351f765a29
yanghaiji <hai.ji.yang@ibm.com>
2022-09-18 23:09:04 +0800

    Initial commit

---
+++ data_type/number.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+<p>
+
+</p>
+@author: hai ji
+@file: number.py
+@date: 2022/9/18 
+"""
+
+if __name__ == '__main__':
+    num1: int = 12
+    print("num1 的值为 : %d" % num1)
+
+    num2 = 24  # 这里我们可以简写
+
+    sum_num = num1 + num2
+    print("num1 + num2 = %d" % sum_num)
+
+    sub_num = num2 - num1
+    print("num2 - num12 =  %d" % sub_num)
+
+    mul_num = num2 * num1
+    print("num2 - num12 =  %d" % mul_num)
+
+    div_num = num2 / num1
+    print("num2 - num12 =  %d" % div_num)
---
+++ helloworld/hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+<p>
+
+</p>
+@author: hai ji
+@file: hello_world.py
+@date: 2022/9/18 
+"""
+
+if __name__ == '__main__':
+    print("hello world!")
