f9f6a4502bebddaf9c569840d2aa209ef4fdf5dc
yanghaiji <hai.ji.yang@ibm.com>
2022-10-05 13:42:46 +0800

    add ERROR :new:

---
+++ models/model_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+<p>
+
+</p>
+@author: hai ji
+@file: model_test.py
+@date: 2022/9/22 
+"""
+from models.fibo import fibo01
+
+print(fibo01("1234"))
+print(fibo01("basketball"))
+
+print(dir(fibo01(1234)))
+
+print(dir())
